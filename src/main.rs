6ebd38df9c1591131b8cc18bfec5c63928251eb2
ivario123 <ivar.jonsson18@gmail.com>
2024-03-17 10:06:23 +0100

    remove symex

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, fmt::Display, fs, time::Duration};
 
 use object::{Object, ObjectSymbol};
 use probe_rs::{flashing, Core, MemoryInterface, Permissions, Session};
-use symex::{general_assembly::RunConfig, run_elf};
+// use symex::{general_assembly::RunConfig, run_elf};
 
 struct Measurement {
     name: String,
@@ -19,9 +19,9 @@ impl Display for Measurement {
 fn main() {
     println!("Utility to measure HW cycles for the nRF52840_xxAA");
 
-    // let mut session = Session::auto_attach("nRF52840_xxAA", Permissions::default()).unwrap();
+    let mut session = Session::auto_attach("nRF52840_xxAA", Permissions::default()).unwrap();
 
-    // println!("attached to nRF52840_xxAA {:?}", session.architecture());
+    println!("attached to nRF52840_xxAA {:?}", session.architecture());
 
     println!("name\t\thw\tsymex");
     for to_test in fs::read_dir("test_binarys").unwrap() {
@@ -32,9 +32,9 @@ fn main() {
             let path = to_test.unwrap().path();
             let path_str = path.to_string_lossy().to_string();
             let name = path_str.split('/').last().unwrap();
-            // let hw_measurement = measure_hw(&path_str, &mut session);
+            let hw_measurement = measure_hw(&path_str, &mut session);
 
-            let symex_measurement = measure_symex(&path_str);
+            // let symex_measurement = measure_symex(&path_str);
 
             // let measurement = Measurement {
             //     name: name.to_owned(),
@@ -42,29 +42,29 @@ fn main() {
             //     symex: symex_measurement,
             // };
 
-            println!("Name : {name} \n\thw \t: \n\tsymex \t: {symex_measurement} ");
+            println!("Name : {name} \n\thw \t: {hw_measurement} \n\tsymex \t:  ");
         // }
     }
 }
-
-fn measure_symex(path: &str) -> u64 {
-    let cfg = RunConfig {
-        show_path_results: false,
-        pc_hooks: vec![],
-        register_read_hooks: vec![],
-        register_write_hooks: vec![],
-        memory_write_hooks: vec![],
-        memory_read_hooks: vec![],
-    };
-    let results = run_elf::run_elf(path, "measure", cfg).unwrap();
-    let mut max = 0;
-
-    for result in results {
-        max = max.max(result.max_cycles);
-    }
-
-    max as u64
-}
+//
+// fn measure_symex(path: &str) -> u64 {
+//     let cfg = RunConfig {
+//         show_path_results: false,
+//         pc_hooks: vec![],
+//         register_read_hooks: vec![],
+//         register_write_hooks: vec![],
+//         memory_write_hooks: vec![],
+//         memory_read_hooks: vec![],
+//     };
+//     let results = run_elf::run_elf(path, "measure", cfg).unwrap();
+//     let mut max = 0;
+//
+//     for result in results {
+//         max = max.max(result.max_cycles);
+//     }
+//
+//     max as u64
+// }
 
 fn measure_hw(path: &str, session: &mut Session) -> u64 {
     flashing::download_file(session, path, flashing::Format::Elf).unwrap();
